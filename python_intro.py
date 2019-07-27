71a58ece9de00021d93ca13ef9a6e39b56f0106e
Ruben Dario Araiza Acosta <raraiza@nearsoft.com>
2019-07-27 16:47:08 -0500

    se agregan templates

---
+++ python_intro.py
@@ -0,0 +1,19 @@
+name = "Ruben Dario Araiza Acosta"
+
+print("Hello World! me llamo " + name)
+
+if 3 > 1:
+    print('3 es mayor que 1')
+
+
+if 4 > 100:
+    print('4 es mayor que 100')
+else:
+    print('4 es NO mayor que 100')
+
+segundo_nombre = 'Dario'
+if segundo_nombre in name:
+    print("tambien me llamo " + segundo_nombre)
+else:
+    print("No me llamo " + segundo_nombre)
+
